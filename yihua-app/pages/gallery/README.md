6206956016f2081ca350e257a075e550c0c5fa4d
Yi-Hua Liang <yihua1218@gmail.com>
2018-06-14 08:41:11 +0800

    Add bootstrap-social

---
+++ yihua-app/pages/gallery/README.md
@@ -0,0 +1,4 @@
+# Social Buttons for Bootstrap
+
+1. [Social Buttons for Bootstrap](https://lipis.github.io/bootstrap-social/)
+2. [@fortawesome/vue-fontawesome](https://www.npmjs.com/package/@fortawesome/vue-fontawesome)
---
+++ yihua-app/pages/gallery/bootstrap-social.vue
@@ -0,0 +1,12 @@
+<template lang="pug">
+  div
+    button.btn.btn-primary.btn-sm(type="button") Facebook
+    button.btn.btn-primary.btn-sm(type="button") Microsoft
+    button.btn.btn-primary.btn-sm(type="button") Google
+    button.btn.btn-primary.btn-sm(type="button") WeChat
+    button.btn.btn-primary.btn-sm(type="button") Weibo
+
+    a.btn.btn-block.btn-social.btn-twitter
+      span.fa.fa-twitter Sign in with Twitter
+
+</template>
